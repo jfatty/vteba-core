b5c5d19b817de195835749a0e9fbac298a49c66c
skmbw <tongku2008@126.com>
2014-07-03 16:35:28 +0800

    spring泛型dao使用样例代码，以后用工具生成

--- vteba-core/src/test/java/com/vteba/user/EmpUser.java
+++ vteba-core/src/test/java/com/vteba/user/EmpUser.java
@@ -1,5 +1,23 @@
 package com.vteba.user;
 
 public class EmpUser {
-
+    private String userName;
+    private Integer age;
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
 }
--- vteba-core/src/test/java/com/vteba/user/UserDaoImpl.java
+++ vteba-core/src/test/java/com/vteba/user/UserDaoImpl.java
@@ -1,8 +1,19 @@
 package com.vteba.user;
 
-import com.vteba.tx.jdbc.spring.impl.SpringGenericDaoImpl;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
-public class UserDaoImpl extends SpringGenericDaoImpl<EmpUser, Long> {
+import com.google.common.collect.Maps;
+import com.vteba.tx.jdbc.spring.SpringJdbcTemplate;
+import com.vteba.tx.jdbc.spring.impl.AbstractSpringGenericDao;
+
+/**
+ * spring generic dao使用例子，以后会使用代码生成工具生成。
+ * @author  yinlei 
+ * @since 2014-7-2
+ */
+public class UserDaoImpl extends AbstractSpringGenericDao<EmpUser, Long> {
 
 	public UserDaoImpl() {
 		super(EmpUser.class);
@@ -11,5 +22,45 @@ public class UserDaoImpl extends SpringGenericDaoImpl<EmpUser, Long> {
 	public UserDaoImpl(Class<EmpUser> entityClass) {
 		super(entityClass);
 	}
+
+    @Override
+    public void setSpringJdbcTemplate(SpringJdbcTemplate skmbwJdbcTemplate) {
+        this.springJdbcTemplate = skmbwJdbcTemplate;
+    }
+
+    // 完全可以使用代码生成工具来生成
+    @Override
+    public EmpUser mapRows(ResultSet rs, int rowNum) throws SQLException {
+        EmpUser user = new EmpUser();
+        user.setUserName(rs.getString("user_name"));
+        user.setAge(rs.getInt("age"));
+        return user;
+    }
+
+    // 完全可以使用代码生成工具来生成
+    @Override
+    public Map<String, Object> mapBean(EmpUser entity, boolean prefix, Map<String, Object> params) {
+        Map<String, Object> resultMap = Maps.newHashMap();
+        if (entity != null) {
+            if (prefix) {
+                if (entity.getUserName() != null) {
+                    resultMap.put("getUserName", entity.getUserName());
+                    params.put("getUserName", entity.getUserName());
+                }
+                if (entity.getAge() != null) {
+                    resultMap.put("getAge", entity.getAge());
+                    params.put("getAge", entity.getAge());
+                }
+            } else {
+                if (entity.getUserName() != null) {
+                    resultMap.put("userName", entity.getUserName());
+                }
+                if (entity.getAge() != null) {
+                    resultMap.put("age", entity.getAge());
+                }
+            }
+        }
+        return resultMap;
+    }
 	
 }
