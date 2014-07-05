88c1d56c34d071867fa7513f2507f299952441b4
skmbw <tongku2008@126.com>
2014-07-05 21:27:33 +0800

    spring 通用泛型service

---
+++ vteba-core/src/main/java/com/vteba/service/generic/impl/CommonServiceImpl.java
@@ -0,0 +1,164 @@
+package com.vteba.service.generic.impl;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import com.vteba.service.generic.CommonService;
+import com.vteba.tx.generic.Page;
+import com.vteba.tx.jdbc.spring.spi.SpringGenericDao;
+
+/**
+ * 基于SpringJdbcTemplate的通用泛型Service实现。
+ * @author yinlei
+ * @date 2014-7-5
+ * @param <T> 实体类型
+ * @param <ID> 主键类型
+ */
+public abstract class CommonServiceImpl<T, ID extends Serializable> implements CommonService<T, ID> {
+	protected SpringGenericDao<T, ID> springGenericDaoImpl;
+	
+	public abstract void setSpringGenericDaoImpl(SpringGenericDao<T, ID> springGenericDaoImpl);
+
+	public ID save(T entity) {
+		return springGenericDaoImpl.save(entity);
+	}
+
+	public int update(T entity) {
+		return springGenericDaoImpl.update(entity);
+	}
+
+	public int updateBatch(T entity, T criteria) {
+		return springGenericDaoImpl.updateBatch(entity, criteria);
+	}
+
+	public int updateBatch(T entity, Map<String, Object> params) {
+		return springGenericDaoImpl.updateBatch(entity, params);
+	}
+
+	public int updateBatch(String sql, Map<String, Object> params) {
+		return springGenericDaoImpl.updateBatch(sql, params);
+	}
+
+	public int updateBatch(String sql, T params) {
+		return springGenericDaoImpl.updateBatch(sql, params);
+	}
+
+	public int updateBatch(String sql, Object... params) {
+		return springGenericDaoImpl.updateBatch(sql, params);
+	}
+
+	public T get(ID id) {
+		return springGenericDaoImpl.get(id);
+	}
+
+	public T unique(T entity) {
+		return springGenericDaoImpl.unique(entity);
+	}
+
+	public T unique(Map<String, Object> params) {
+		return springGenericDaoImpl.unique(params);
+	}
+
+	public int delete(ID id) {
+		return springGenericDaoImpl.delete(id);
+	}
+
+	public int deleteBatch(T entity) {
+		return springGenericDaoImpl.deleteBatch(entity);
+	}
+
+	public int deleteBatch(String sql, T entity) {
+		return springGenericDaoImpl.deleteBatch(sql, entity);
+	}
+
+	public int deleteBatch(String sql, Map<String, Object> params) {
+		return springGenericDaoImpl.deleteBatch(sql, params);
+	}
+
+	public int deleteBatch(String sql, Object... params) {
+		return springGenericDaoImpl.deleteBatch(sql, params);
+	}
+
+	public List<T> query(T entity) {
+		return springGenericDaoImpl.query(entity);
+	}
+
+	public List<T> query(Map<String, Object> params) {
+		return springGenericDaoImpl.query(params);
+	}
+
+	public List<T> query(String sql, T params) {
+		return springGenericDaoImpl.query(sql, params);
+	}
+
+	public List<T> query(String sql, Map<String, Object> params) {
+		return springGenericDaoImpl.query(sql, params);
+	}
+
+	public List<T> query(String sql, Object... params) {
+		return springGenericDaoImpl.query(sql, params);
+	}
+
+	public <VO> List<VO> queryList(String sql, VO params) {
+		return springGenericDaoImpl.queryList(sql, params);
+	}
+
+	public <VO> List<VO> queryList(String sql, Class<VO> resultClass,
+			Map<String, Object> params) {
+		return springGenericDaoImpl.queryList(sql, resultClass, params);
+	}
+
+	public <VO> List<VO> queryList(String sql, Class<VO> resultClass,
+			Object... params) {
+		return springGenericDaoImpl.queryList(sql, resultClass, params);
+	}
+
+	public Page<T> queryForPage(Page<T> page, T params) {
+		return springGenericDaoImpl.queryForPage(page, params);
+	}
+
+	public Page<T> queryForPage(Page<T> page, String sql, T params) {
+		return springGenericDaoImpl.queryForPage(page, sql, params);
+	}
+
+	public Page<T> queryForPage(Page<T> page, Map<String, Object> params) {
+		return springGenericDaoImpl.queryForPage(page, params);
+	}
+
+	public Page<T> queryForPage(Page<T> page, String sql,
+			Map<String, Object> params) {
+		return springGenericDaoImpl.queryForPage(page, sql, params);
+	}
+
+	public Page<T> queryForPage(Page<T> page, String sql, Object... params) {
+		return springGenericDaoImpl.queryForPage(page, sql, params);
+	}
+
+	public <VO> Page<VO> queryPageList(Page<VO> page, String sql, VO params) {
+		return springGenericDaoImpl.queryPageList(page, sql, params);
+	}
+
+	public <VO> Page<VO> queryPageList(Page<VO> page, String sql,
+			Class<VO> resultClass, Map<String, Object> params) {
+		return springGenericDaoImpl.queryPageList(page, sql, resultClass,
+				params);
+	}
+
+	public <VO> Page<VO> queryPageList(Page<VO> page, String sql,
+			Class<VO> resultClass, Object... params) {
+		return springGenericDaoImpl.queryPageList(page, sql, resultClass,
+				params);
+	}
+
+	public <X> X queryForObject(String sql, Map<String, ?> paramMap,
+			Class<X> requiredType) {
+		return springGenericDaoImpl.queryForObject(sql, paramMap, requiredType);
+	}
+
+	public <X> X queryForObject(String sql, Class<X> requiredType,
+			Object... params) {
+		return springGenericDaoImpl.queryForObject(sql, requiredType, params);
+	}
+
+}
