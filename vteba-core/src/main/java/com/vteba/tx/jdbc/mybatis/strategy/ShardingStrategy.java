9ce62d7a0455870efdb619e77550a60a6be315b9
skmbw <tongku2008@126.com>
2014-09-17 09:46:19 +0800

    添加注释

--- vteba-core/src/main/java/com/vteba/tx/jdbc/mybatis/strategy/ShardingStrategy.java
+++ vteba-core/src/main/java/com/vteba/tx/jdbc/mybatis/strategy/ShardingStrategy.java
@@ -6,5 +6,13 @@ package com.vteba.tx.jdbc.mybatis.strategy;
  * @since 2013-12-9
  */
 public interface ShardingStrategy {
+    
+    /**
+     * 表名变更策略。
+     * @param baseTableName 原表名
+     * @param paramObject sql参数
+     * @param mapperId mybatis sql映射的id
+     * @return 新的表名
+     */
     public String getTargetTableName(String baseTableName, Object paramObject, String mapperId);
 }
