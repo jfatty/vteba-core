2ce03582c283330406f6541c1a38ff995468cc0f
skmbw <tongku2008@126.com>
2014-09-29 11:14:38 +0800

    拦截schema mybatis

--- vteba-core/src/main/java/com/vteba/service/multitenant/interceptor/DetermineSchemaInterceptor.java
+++ vteba-core/src/main/java/com/vteba/service/multitenant/interceptor/DetermineSchemaInterceptor.java
@@ -2,6 +2,8 @@ package com.vteba.service.multitenant.interceptor;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vteba.service.multitenant.SchemaContextHolder;
 import com.vteba.service.multitenant.annotation.Schema;
@@ -12,16 +14,26 @@ import com.vteba.service.multitenant.annotation.Schema;
  * date 2012-8-16 下午9:33:59
  */
 public class DetermineSchemaInterceptor implements MethodInterceptor{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DetermineSchemaInterceptor.class);
 
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		Object object = invocation.getThis();//获得被拦截的目标对象
-		boolean isSchema = object.getClass().isAnnotationPresent(Schema.class);
-		if (isSchema) {
-			Schema schema = object.getClass().getAnnotation(Schema.class);
-			String schemaName = schema.schemaName();
-			//System.out.println("拦截器打印的，Schema是：" + schemaName + "，线程是：" + Thread.currentThread());
-			SchemaContextHolder.putSchema(schemaName);
+		//boolean isSchema = object.getClass().isAnnotationPresent(Schema.class);// 拦截类，这种方式是可以的
+		Class<?> proxyClass = object.getClass();
+		Class<?>[] classes = proxyClass.getInterfaces();// 因为我们知道，代理的就是MyBatis的Mapper借口
+		if (classes != null) {
+			Class<?> clazz = classes[0];
+			//boolean isSchema = clazz.isAnnotationPresent(Schema.class);// 不用判断了，这个方法底层调用的也是getAnnotation
+			Schema schema = clazz.getAnnotation(Schema.class);
+			if (schema != null) {
+				String schemaName = schema.schemaName();
+				if (LOGGER.isInfoEnabled()) {
+					LOGGER.info("Schema拦截器打印的，Schema是：[{}]，线程name是：[{}]。", schemaName, Thread.currentThread().getName());
+				}
+				SchemaContextHolder.putSchema(schemaName);
+			}
 		}
 		return invocation.proceed();
 	}
