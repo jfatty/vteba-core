ef138f2f36fc7de3a3de20a3b91d7d1a6cec3069
skmbw <tongku2008@126.com>
2014-11-11 15:53:50 +0800

    因为channel是串行的，应该不需要同步

--- vteba-core/src/main/java/com/vteba/socket/netty/LengthBasedFrameDecoder.java
+++ vteba-core/src/main/java/com/vteba/socket/netty/LengthBasedFrameDecoder.java
@@ -1,15 +1,15 @@
 package com.vteba.socket.netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
 import java.util.List;
 
 import com.vteba.utils.annotation.ThreadSafe;
 import com.vteba.utils.charstr.ByteUtils;
 import com.vteba.utils.charstr.Char;
 import com.vteba.utils.zip.ZipUtils;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
 
 /**
  * 基于头长度的netty decoder。netty原生的实现长度有限制，且不能喝解压一起使用。
@@ -23,7 +23,8 @@ public class LengthBasedFrameDecoder extends ByteToMessageDecoder {
 	private final boolean headerHex;
 	private final boolean uncompress;
 	
-	private ThreadLocal<Integer> frameLengthLocal = new ThreadLocal<Integer>();
+	//private ThreadLocal<Integer> frameLengthLocal = new ThreadLocal<Integer>();
+	private Integer frameLength = null;
 	
 	/**
 	 * 构造一个实例。
@@ -41,8 +42,8 @@ public class LengthBasedFrameDecoder extends ByteToMessageDecoder {
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		int readable = in.readableBytes();
 		if (readable >= headerLength) {
-			Integer contentLength = frameLengthLocal.get();
-			if (contentLength == null) {
+			//Integer frameLength = frameLengthLocal.get();
+			if (frameLength == null) {
 				byte[] header = new byte[headerLength];
 				in.readBytes(header);
 				int length = 0;
@@ -51,9 +52,10 @@ public class LengthBasedFrameDecoder extends ByteToMessageDecoder {
 				} else {
 					length = ByteUtils.toInt(header);
 				}
-				frameLengthLocal.set(length);
-			} else if (contentLength <= readable) {// 如果可读的数据大于内容长度，说明粘包了，只取内容长度的字节
-				byte[] dest = new byte[contentLength];
+				//frameLengthLocal.set(length);
+				frameLength = length;
+			} else if (frameLength <= readable) {// 如果可读的数据大于内容长度，说明粘包了，只取内容长度的字节
+				byte[] dest = new byte[frameLength];
 				in.readBytes(dest);
 				String result = null;
 				if (uncompress) {
@@ -62,7 +64,8 @@ public class LengthBasedFrameDecoder extends ByteToMessageDecoder {
 					result = new String(dest, Char.UTF8);
 				}
 				out.add(result);
-				frameLengthLocal.remove();
+				//frameLengthLocal.remove();
+				frameLength = null;
 			}
 		}
 	}
